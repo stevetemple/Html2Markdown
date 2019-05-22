fddb913e52060f0ff31858b7b59263bb2acfbbd3
Simon Baynes <baynezy@gmail.com>
2019-05-22 22:43:16 +0100

    Testing

--- uploadTestResults.ps1
+++ uploadTestResults.ps1
@@ -1,16 +1,13 @@
 cd $env:APPVEYOR_BUILD_FOLDER
-
-cd test
-
-cd Html2Markdown.Test
-
-cd TestResults
-
-ls
+cd test\Html2Markdown.Test
 
 Write-Host "- About to upload test results"
 
 $wc = New-Object "System.Net.WebClient"
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/mstest/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\test\Html2Markdown.Test\TestResults\results.trx))
+
+Get-ChildItem -Path *.trx |
+Foreach-Object {
+    $wc.UploadFile("https://ci.appveyor.com/api/testresults/mstest/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $_.FullName))
+}
 
 Write-Host "- Results uploaded"
